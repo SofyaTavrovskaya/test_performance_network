ff3a785adb101005d36cd304db5ed2df903501b7
SofyaTavrovskaya <tavrovskayasofya87@gmail.com>
2019-05-02 18:02:22 +0300

    add function to get pairs of vms

--- get_pairs_of_vms.py
+++ get_pairs_of_vms.py
@@ -1,20 +1,25 @@
 #!/usr/bin/python3
 
-vms_with_host1_interfaces = {'ubuntu16.04-1': {'ip': '192.168.14.137', 'mac': '52:54:00:8e:1a:e4', 'host_interface': 'enp1s0'},
-                            'ubuntu16.04-2': {'ip': '192.168.14.136', 'mac': 'fe:54:00:8e:1a:e4', 'host_interface': 'enp2s0'},
-                            'ubuntu16.04-3': {'ip': '192.168.14.135', 'mac': 'fe:54:01:8e:1a:e4', 'host_interface': 'enp3s0'},
-                            'ubuntu16.04-4': {'ip': '192.168.14.134', 'mac': 'fe:54:02:8e:1a:e4', 'host_interface': 'enp4s0'}}
+import itertools
+#
+# vms_with_host1_interfaces = {'ubuntu16.04-1': {'ip': '192.168.14.137', 'mac': '52:54:00:8e:1a:e4', 'host_interface': 'enp1s0'},
+#                             'ubuntu16.04-2': {'ip': '192.168.14.136', 'mac': 'fe:54:00:8e:1a:e4', 'host_interface': 'enp2s0'},
+#                             'ubuntu16.04-3': {'ip': '192.168.14.135', 'mac': 'fe:54:01:8e:1a:e4', 'host_interface': 'enp3s0'},
+#                             'ubuntu16.04-4': {'ip': '192.168.14.134', 'mac': 'fe:54:02:8e:1a:e4', 'host_interface': 'enp4s0'}}
+#
+#
+# vms_with_host2_interfaces = {'ubuntu18.04-1': {'ip': '192.168.14.147', 'mac': '52:55:00:8e:1a:e4', 'host_interface': 'enp5s0'},
+#                             'ubuntu18.04-2': {'ip': '192.168.14.146', 'mac': 'fe:54:03:7e:1a:e4', 'host_interface': 'enp6s0'},
+#                             'ubuntu18.04-3': {'ip': '192.168.14.145', 'mac': 'fe:54:04:7e:1a:e4', 'host_interface': 'enp7s0'},
+#                              'ubuntu18.04-4': {'ip': '192.168.14.144', 'mac': 'fe:54:05:7e:1a:e4', 'host_interface': 'enp8s0'},
+#                              'ubuntu18.04-5': {'ip': '192.168.14.144', 'mac': 'fe:54:05:7e:1a:e4', 'host_interface': 'enp6s0'}}
+#
+# connections = [{'host1': 'enp1s0', 'host2': 'enp8s0'}, {'host1': 'enp2s0', 'host2': 'enp6s0'}, {'host1': 'enp3s0', 'host2': 'enp5s0'}, {'host1': 'enp4s0', 'host2': 'enp7s0'}]
 
 
-vms_with_host2_interfaces = {'ubuntu18.04-1': {'ip': '192.168.14.147', 'mac': '52:55:00:8e:1a:e4', 'host_interface': 'enp5s0'},
-                            'ubuntu18.04-2': {'ip': '192.168.14.146', 'mac': 'fe:54:03:7e:1a:e4', 'host_interface': 'enp6s0'},
-                            'ubuntu18.04-3': {'ip': '192.168.14.145', 'mac': 'fe:54:04:7e:1a:e4', 'host_interface': 'enp7s0'},
-                             'ubuntu18.04-4': {'ip': '192.168.14.144', 'mac': 'fe:54:05:7e:1a:e4', 'host_interface': 'enp8s0'},
-                             'ubuntu18.04-5': {'ip': '192.168.14.144', 'mac': 'fe:54:05:7e:1a:e4', 'host_interface': 'enp6s0'}}
-
-
-def get_pair_of_vms(vms_with_host1_interfaces, vms_with_host2_interfaces):
-    for vm1 in vms_with_host1_interfaces:
-        for vm2 in vms_with_host2_interfaces:
-            if ((vms_with_host1_interfaces[vm1][vms_with_host1_interfaces] == 'enp1s0' & vms_with_host2_interfaces[vm2][vms_with_host2_interfaces] == 'enp8s0') | \
-                    (vms_with_host1_interfaces[vm1][vms_with_host1_interfaces] == 'enp2s0' and vms_with_host2_interfaces[vm2][vms_with_host2_interfaces] == 'enp6s0'))
+def get_pairs_of_vms(vms_with_host1_interfaces, vms_with_host2_interfaces, connections):
+    vms_pair = []
+    for iteritems in itertools.product(vms_with_host1_interfaces, vms_with_host2_interfaces):
+        if {'host1': vms_with_host1_interfaces[iteritems[0]]['host_interface'], 'host2': vms_with_host2_interfaces[iteritems[1]]['host_interface']} in connections:
+            vms_pair.append({'host1': vms_with_host1_interfaces[iteritems[0]]['ip'], 'host2': vms_with_host2_interfaces[iteritems[1]]['ip']})
+    return vms_pair
--- perf_test_net.py
+++ perf_test_net.py
@@ -4,12 +4,15 @@
 from get_vf_mac_addresses import get_vf_mac_addresses_for_interfaces
 from get_names_of_vms import get_names_of_vms_from_host_machine
 from sort_vms_to_physical_interfaces import sort_vms_to_physical_interfaces
+from get_pairs_of_vms import get_pairs_of_vms
 
 host1_uri = 'qemu:///system'
 host2_uri = 'qemu+ssh://ubuntu@192.168.14.136/system'
 
 host1_list_interfaces = ['enp3s0', 'vnet0', 'vnet1']
 host2_list_interfaces = ['enp3s0', 'vnet0', 'vnet1']
+
+connections = [{'host1': 'enp1s0', 'host2': 'enp8s0'}, {'host1': 'enp2s0', 'host2': 'enp6s0'}, {'host1': 'enp3s0', 'host2': 'enp5s0'}, {'host1': 'enp4s0', 'host2': 'enp7s0'}]
 
 vf_mac_addresses_host1 = get_vf_mac_addresses_for_interfaces(host1_list_interfaces)
 vf_mac_addresses_host2 = get_vf_mac_addresses_for_interfaces(host2_list_interfaces)
@@ -19,6 +22,10 @@ vms_name_host2 = get_names_of_vms_from_host_machine(host2_uri)
 
 vms_with_host1_interfaces = sort_vms_to_physical_interfaces(vf_mac_addresses_host1, vms_name_host1)
 vms_with_host2_interfaces = sort_vms_to_physical_interfaces(vf_mac_addresses_host2, vms_name_host2)
+
+get_pairs_of_vms(vms_with_host1_interfaces, vms_with_host2_interfaces, connections)
+
+
 
 
 
