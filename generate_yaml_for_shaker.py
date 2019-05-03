944cfc6ccc09245c3c381c42bb6c2ea80ced60da
SofyaTavrovskaya <tavrovskayasofya87@gmail.com>
2019-05-03 18:36:15 +0300

    add start of shaker

--- generate_yaml_for_shaker.py
+++ generate_yaml_for_shaker.py
@@ -3,21 +3,24 @@
 import yaml
 
 
-dictionary = {'host2': '192.168.14.145', 'host1': '192.168.14.134'}
+# dictionary = {'host2': '192.168.14.145', 'host1': '192.168.14.134'}
+# input_file = 'template.yaml'
 
 
-def generate_yaml_for_shaker(dictionary):
-    with open('my_file.yaml') as f:
+def generate_yaml_for_shaker(dictionary, template):
+    with open(template) as f:
         doc = yaml.safe_load(f)
 
     doc['deployment']['agents'][0]['ip'] = dictionary['host1']
     doc['deployment']['agents'][1]['ip'] = dictionary['host2']
 
-    with open('my_file.yaml', 'w') as f:
+    output_file = 'shaker-'+dictionary['host1']+'-'+dictionary['host2']+'.yaml'
+
+    with open(output_file, 'w') as f:
         yaml.dump(doc, f, default_flow_style=False)
 
+    return output_file
 
-generate_yaml_for_shaker(dictionary)
 
 
 
--- perf_test_net.py
+++ perf_test_net.py
@@ -1,31 +1,44 @@
 #!/usr/bin/python3
-import uuid
 
-from get_vf_mac_addresses import get_vf_mac_addresses_for_interfaces
-from get_names_of_vms import get_names_of_vms_from_host_machine
-from sort_vms_to_physical_interfaces import sort_vms_to_physical_interfaces
-from get_pairs_of_vms import get_pairs_of_vms
+import subprocess
+
+# from get_vf_mac_addresses import get_vf_mac_addresses_for_interfaces
+# from get_names_of_vms import get_names_of_vms_from_host_machine
+# from sort_vms_to_physical_interfaces import sort_vms_to_physical_interfaces
+# from get_pairs_of_vms import get_pairs_of_vms
 from generate_yaml_for_shaker import generate_yaml_for_shaker
 
-host1_uri = 'qemu:///system'
-host2_uri = 'qemu+ssh://ubuntu@192.168.14.136/system'
+# host1_uri = 'qemu:///system'
+# host2_uri = 'qemu+ssh://ubuntu@192.168.14.136/system'
+#
+# host1_list_interfaces = ['enp3s0', 'vnet0', 'vnet1']
+# host2_list_interfaces = ['enp3s0', 'vnet0', 'vnet1']
+#
+# connections = [{'host1': 'enp1s0', 'host2': 'enp8s0'}, {'host1': 'enp2s0', 'host2': 'enp6s0'}, {'host1': 'enp3s0', 'host2': 'enp5s0'}, {'host1': 'enp4s0', 'host2': 'enp7s0'}]
+#
+# vf_mac_addresses_host1 = get_vf_mac_addresses_for_interfaces(host1_list_interfaces)
+# vf_mac_addresses_host2 = get_vf_mac_addresses_for_interfaces(host2_list_interfaces)
+#
+# vms_name_host1 = get_names_of_vms_from_host_machine(host1_uri)
+# vms_name_host2 = get_names_of_vms_from_host_machine(host2_uri)
+#
+# vms_with_host1_interfaces = sort_vms_to_physical_interfaces(vf_mac_addresses_host1, vms_name_host1)
+# vms_with_host2_interfaces = sort_vms_to_physical_interfaces(vf_mac_addresses_host2, vms_name_host2)
 
-host1_list_interfaces = ['enp3s0', 'vnet0', 'vnet1']
-host2_list_interfaces = ['enp3s0', 'vnet0', 'vnet1']
+list = [{'host2': '10.0.0.4', 'host1': '10.0.0.6'}]
+input_file = 'template.yaml'
 
-connections = [{'host1': 'enp1s0', 'host2': 'enp8s0'}, {'host1': 'enp2s0', 'host2': 'enp6s0'}, {'host1': 'enp3s0', 'host2': 'enp5s0'}, {'host1': 'enp4s0', 'host2': 'enp7s0'}]
+name_of_yaml_files = []
+for item in list:
+    name_of_yaml_files.append(generate_yaml_for_shaker(item, input_file))
 
-vf_mac_addresses_host1 = get_vf_mac_addresses_for_interfaces(host1_list_interfaces)
-vf_mac_addresses_host2 = get_vf_mac_addresses_for_interfaces(host2_list_interfaces)
+print(name_of_yaml_files)
 
-vms_name_host1 = get_names_of_vms_from_host_machine(host1_uri)
-vms_name_host2 = get_names_of_vms_from_host_machine(host2_uri)
+for name in name_of_yaml_files:
+    report_name = 'report-' + name + '.html'
+    subprocess.call(["shaker", "--server-endpoint", "192.168.14.136:8080", "--scenario", name, "--report", report_name])
 
-vms_with_host1_interfaces = sort_vms_to_physical_interfaces(vf_mac_addresses_host1, vms_name_host1)
-vms_with_host2_interfaces = sort_vms_to_physical_interfaces(vf_mac_addresses_host2, vms_name_host2)
 
-for item in get_pairs_of_vms(vms_with_host1_interfaces, vms_with_host2_interfaces, connections):
-    generate_yaml_for_shaker(item)
 
 
 
